1cbf154cc773fb4dc706b54883878c3860c4a2e4
Ravi Ajmera <Ravi.Ajmera@bhnetwork.com>
2017-04-27 13:28:06 -0700

    changed port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 8080;
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);    
